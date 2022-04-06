189fb714aa1316b3454bc4e4598e89d6cac14c94
gabycode <86427446+gabycode@users.noreply.github.com>
2022-04-06 08:54:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,51 @@ let sendInvoice = document.getElementById("send-invoice")
 let totalAmount = document.getElementById("total-amount")
 let note = document.getElementById("note")
 let sum = 0
+
+function renderServices() {
+  let service = ""
+  for(let i = 0; i < myServices.length; i++) {
+    service += `
+        <div class="content">
+        <p class="services">${myServices[i].service}</p> 
+        <p class="price"><span class="dollar">$</span>${myServices[i].price}</p>
+        </div>`
+  }
+  servicesContainer.innerHTML = service
+  note.textContent = "We accept cash, credit card, or PayPal"
+}
+
+washCarBtn.addEventListener("click", function() {
+  myServices.push(washCar)
+  renderServices()
+  washCarBtn.disabled = true
+  sum += washCar.price;
+  totalAmount.textContent = "$" + sum
+})
+
+mowLawnBtn.addEventListener("click", function() {
+  myServices.push(mowLawn)
+  renderServices()
+  mowLawnBtn.disabled = true
+  sum += mowLawn.price;
+  totalAmount.textContent = "$" + sum
+})
+
+pullWeedsBtn.addEventListener("click", function() {
+  myServices.push(pullWeeds)
+  renderServices()
+  pullWeedsBtn.disabled = true
+  sum += pullWeeds.price;
+  totalAmount.textContent = "$" + sum
+})
+
+sendInvoice.addEventListener("click", function(){
+  sum = 0
+  totalAmount.textContent = "$" + sum
+  myServices = []
+  note.textContent = ""
+  pullWeedsBtn.disabled = false
+  washCarBtn.disabled = false
+  mowLawnBtn.disabled = false
+  servicesContainer.textContent = ""
+})
